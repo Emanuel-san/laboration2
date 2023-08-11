e2b36893ba5045cc1c23abeb92ac209984592740
Emanuel-san <emanuel.sanchez@ha.ax>
2023-08-11 23:04:49 +0300

    Initial commit with open api generator plugin configuration

---
+++ src/main/java/lab2/webshop/controllers/CustomProductsDelegate.java
@@ -0,0 +1,31 @@
+package lab2.webshop.controllers;
+
+import lab2.webshop.openapi.api.ProductsApiDelegate;
+import lab2.webshop.openapi.model.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CustomProductsDelegate implements ProductsApiDelegate {
+
+    public CustomProductsDelegate(){}
+
+    @Override
+    public ResponseEntity<List<Product>> getProducts(String name){
+        List<Product> list = new ArrayList<>();
+        Product prd = new Product("PRD-1", name, BigDecimal.valueOf(32.2), "A magical product", "imageurl");
+        list.add(prd);
+
+        return ResponseEntity.ok(list);
+    }
+
+    @Override
+    public ResponseEntity<Product> createProduct(Product product){
+       return ResponseEntity.ok(product);
+    }
+
+}

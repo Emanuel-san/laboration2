99fb296a3c9c511ac127836233e91cd10c8e6ac1
Emanuel-san <emanuel.sanchez@ha.ax>
2023-08-28 13:56:25 +0300

    removed unused annotation that created bean issues

--- src/main/java/lab2/webshop/WebshopApplication.java
+++ src/main/java/lab2/webshop/WebshopApplication.java
@@ -2,10 +2,8 @@ package lab2.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class WebshopApplication {
 
     public static void main(String[] args) {

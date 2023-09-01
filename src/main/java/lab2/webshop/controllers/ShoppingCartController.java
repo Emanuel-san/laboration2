22bf73943c54f5c085bdecc6c561b1ebbd32bce0
Emanuel-san <emanuel.sanchez@ha.ax>
2023-09-01 21:12:50 +0300

    Added placer method to controller

--- src/main/java/lab2/webshop/controllers/ShoppingCartController.java
+++ src/main/java/lab2/webshop/controllers/ShoppingCartController.java
@@ -1,4 +1,33 @@
 package lab2.webshop.controllers;
 
-public class ShoppingCartController {
+import lab2.webshop.openapi.api.ShoppingCartsApi;
+import lab2.webshop.openapi.model.CartSummary;
+import lab2.webshop.openapi.model.ShoppingCart;
+import lab2.webshop.openapi.model.ShoppingCartEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ShoppingCartController implements ShoppingCartsApi {
+
+    @Override
+    public ResponseEntity<CartSummary> createShoppingCart(ShoppingCartEntity shoppingCartEntity) {
+        return ResponseEntity.ok(new CartSummary());
+    }
+    @Override
+    public ResponseEntity<CartSummary> getShoppingCart(String productId) {
+        System.out.println(productId);
+        return ResponseEntity.ok(new CartSummary());
+    }
+    @Override
+    public ResponseEntity<List<CartSummary>> getShoppingCarts() {
+        return ResponseEntity.ok(new ArrayList<>());
+    }
+    @Override
+    public ResponseEntity<CartSummary> updateShoppingCart(String shoppingCartId, ShoppingCart shoppingCart) {
+        return ResponseEntity.ok(new CartSummary());
+    }
 }

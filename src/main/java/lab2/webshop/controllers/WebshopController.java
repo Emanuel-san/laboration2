0a9dd1be03b3db8cc1c7afe115af4262cbb68600
Emanuel-san <emanuel.sanchez@ha.ax>
2023-09-14 21:17:46 +0300

    Added mongodb session configuration

--- src/main/java/lab2/webshop/controllers/WebshopController.java
+++ src/main/java/lab2/webshop/controllers/WebshopController.java
@@ -1,16 +1,15 @@
 package lab2.webshop.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lab2.webshop.services.WebshopFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/page")
 public class WebshopController {
 
     WebshopFacade webshopFacade;
@@ -18,10 +17,18 @@ public class WebshopController {
     public WebshopController(WebshopFacade webshopFacade){
         this.webshopFacade = webshopFacade;
     }
-    @GetMapping("/list")
-    public String index(@RequestParam(name="name", required = false, defaultValue = "World")String name,
+
+    @GetMapping ("/")
+    public String home(HttpServletRequest request){
+        request.getSession();
+        return "index";
+    }
+    @GetMapping("/page/list")
+    public String list(@RequestParam(name="name", required = false, defaultValue = "World")String name,
                         Model model,
                         HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        System.out.println(session);
         model.addAttribute("product", webshopFacade.getOneProduct("prd1"));
         return "list";
     }
